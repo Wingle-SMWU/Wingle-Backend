c825034a7ad9d6ce2e9058fc5a37cf201643f254
KimRiun <kimriun27@gmail.com>
2022-12-25 20:53:20 +0900

    [init] 스트링부트 프로젝트 생성

---
+++ wingle/src/main/java/kr/co/wingle/WingleApplication.java
@@ -0,0 +1,13 @@
+package kr.co.wingle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WingleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WingleApplication.class, args);
+	}
+
+}
---
+++ wingle/src/test/java/kr/co/wingle/WingleApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.wingle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WingleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
