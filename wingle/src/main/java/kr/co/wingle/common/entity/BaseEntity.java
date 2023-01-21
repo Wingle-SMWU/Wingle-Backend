5d789cd27516448898ba3323da6d11a4801e479f
silver <silver36c@gmail.com>
2023-01-22 03:25:59 +0900

    [feat] BaseEntity 작성 #9

---
+++ wingle/src/main/java/kr/co/wingle/common/entity/BaseEntity.java
@@ -0,0 +1,50 @@
+package kr.co.wingle.common.entity;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.Where;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.Getter;
+
+@Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@Where(clause = "is_deleted = false")
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id", unique = true, nullable = false)
+	@Getter
+	private Long id;
+
+	@Column(name = "created_time", updatable = false)
+	@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+	@CreatedDate
+	@Getter
+	private LocalDateTime createdTime;
+
+	@Column(name = "updated_time")
+	@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+	@LastModifiedDate
+	@Getter
+	private LocalDateTime updatedTime;
+
+	@Column(name = "is_deleted", columnDefinition = "TINYINT", length = 1)
+	@ColumnDefault("0")
+	private Boolean isDeleted;
+}
