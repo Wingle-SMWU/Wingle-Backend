0d1e1c097fe39f001730b2a20c095821d3ce23bc
silver <silver36c@gmail.com>
2023-01-25 23:11:19 +0900

    [feat] Application에 JpaAuditing 추가 #9
    - BaseEntity의 EntityListner 동작을 위해 필요
    - 이를 통해 @CreatedDate, @LastModifiedDate 사용 가능

--- wingle/src/main/java/kr/co/wingle/WingleApplication.java
+++ wingle/src/main/java/kr/co/wingle/WingleApplication.java
@@ -2,7 +2,9 @@ package kr.co.wingle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class WingleApplication {
 
