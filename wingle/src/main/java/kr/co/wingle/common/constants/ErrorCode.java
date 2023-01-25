938ef55276e597bb15bbabb4c4736203606074a8
silver <silver36c@gmail.com>
2023-01-25 23:08:43 +0900

    [style] ErrorCode import 위치 수정 #9
    - 코딩컨벤션 자동 정렬에 따름

--- wingle/src/main/java/kr/co/wingle/common/constants/ErrorCode.java
+++ wingle/src/main/java/kr/co/wingle/common/constants/ErrorCode.java
@@ -1,16 +1,18 @@
 package kr.co.wingle.common.constants;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
+import static org.springframework.http.HttpStatus.*;
+
 import org.springframework.http.HttpStatus;
 
-import static org.springframework.http.HttpStatus.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-    EXAMPLE_ERROR(BAD_REQUEST, "예시 실패");
+	EXAMPLE_ERROR(BAD_REQUEST, "예시 실패"),
+	NO_ID(BAD_REQUEST, "존재하지 않는 id 입니다");
 
-    private final HttpStatus status;
-    private final String message;
+	private final HttpStatus status;
+	private final String message;
 }
