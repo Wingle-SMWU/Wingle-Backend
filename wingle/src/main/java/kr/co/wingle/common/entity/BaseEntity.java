1b87bf8082ac555bb20b8c79d5b0cc638e448dd0
silver <silver36c@gmail.com>
2023-01-26 00:32:08 +0900

    [feat] BaseEntity가 시간 관련 컬럼만 갖게 수정 #9
    - 기존 BaseEntity는 id, isDeleted도 갖고 있음
    - id 상속시키려면 strategy.AUTO를 사용해야 하는데
      이는 각 테이블이 이어지는 id를 갖게 함
    - isDeleted는 상속시킨 자식 엔티티에서 다른 옵션을
      추가해야 하는데 상속시키면 이를 잊기 쉬우며,
      관련된 옵션은 자식 쪽에 모여있는 것이 합당하다고 판단

--- wingle/src/main/java/kr/co/wingle/common/entity/BaseEntity.java
+++ wingle/src/main/java/kr/co/wingle/common/entity/BaseEntity.java
@@ -3,16 +3,9 @@ package kr.co.wingle.common.entity;
 import java.time.LocalDateTime;
 
 import javax.persistence.Column;
-import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Inheritance;
-import javax.persistence.InheritanceType;
+import javax.persistence.MappedSuperclass;
 
-import org.hibernate.annotations.ColumnDefault;
-import org.hibernate.annotations.Where;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -20,18 +13,9 @@ import org.springframework.format.annotation.DateTimeFormat;
 
 import lombok.Getter;
 
-@Entity
-@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
-@Where(clause = "is_deleted = false")
+@MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
-
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	@Column(name = "id", unique = true, nullable = false)
-	@Getter
-	private Long id;
-
 	@Column(name = "created_time", updatable = false, nullable = false)
 	@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
 	@CreatedDate
@@ -44,7 +28,4 @@ public abstract class BaseEntity {
 	@Getter
 	private LocalDateTime updatedTime;
 
-	@Column(name = "is_deleted", columnDefinition = "TINYINT", length = 1, nullable = false)
-	@ColumnDefault("0")
-	private Boolean isDeleted = false;
 }
