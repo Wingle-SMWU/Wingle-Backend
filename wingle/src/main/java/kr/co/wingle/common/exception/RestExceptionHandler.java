7bf1b4443f7294446b59322fb891199f4bc113bd
yujin113 <claire9585@sookmyung.ac.kr>
2023-01-30 22:47:01 +0900

    [feat] ExceptionHandler 예외, 로깅 추가 #8

--- wingle/src/main/java/kr/co/wingle/common/exception/RestExceptionHandler.java
+++ wingle/src/main/java/kr/co/wingle/common/exception/RestExceptionHandler.java
@@ -2,31 +2,60 @@ package kr.co.wingle.common.exception;
 
 import javax.servlet.http.HttpServletRequest;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import kr.co.wingle.common.constants.ErrorCode;
+import kr.co.wingle.common.dto.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
+
+import org.slf4j.MDC;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import kr.co.wingle.common.dto.ApiResponse;
-import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestControllerAdvice
 public class RestExceptionHandler {
 
 	@ExceptionHandler(value = {CustomException.class})
-	protected ApiResponse<Object> handleCustomException(CustomException e, HttpServletRequest request) {
-		log.info(String.format("[%s Error] : %s %s", e.getErrorCode().getStatus(), request.getMethod(),
-			request.getRequestURI()));
-		return ApiResponse.error(e.getErrorCode());
+	protected ApiResponse<Object> handleCustomException(CustomException exception, HttpServletRequest request) {
+		logInfo(request, exception.getErrorCode().getStatus().toString(), exception.getErrorCode().getMessage());
+		return ApiResponse.error(exception.getErrorCode());
 	}
 
 	// @RequestBody valid 에러
 	@ExceptionHandler(value = {MethodArgumentNotValidException.class})
-	protected ApiResponse<Object> handleMethodArgNotValidException(MethodArgumentNotValidException e,
+	protected ApiResponse<Object> handleMethodArgNotValidException(MethodArgumentNotValidException exception,
 		HttpServletRequest request) {
-		log.info(String.format("[400 Error] : %s %s", request.getMethod(), request.getRequestURI()));
+		logInfo(request, "400 BAD_REQUEST", exception.getBindingResult().getAllErrors().get(0).getDefaultMessage());
 		return ApiResponse.error(HttpStatus.BAD_REQUEST,
-			e.getBindingResult().getAllErrors().get(0).getDefaultMessage());
+			exception.getBindingResult().getAllErrors().get(0).getDefaultMessage());
+	}
+
+	@ExceptionHandler(Exception.class)
+	public ApiResponse<Object> unhandledExceptionHandler(Exception exception, HttpServletRequest request) {
+		logWarn(request, exception);
+		return ApiResponse.error(ErrorCode.SERVER_ERROR);
+	}
+
+	private void logInfo(HttpServletRequest request, String errorCode, String message) {
+		log.info("{} {} : {} - {} (traceId: {})", request.getMethod(), request.getRequestURI(), errorCode,
+			message, getTraceId());
+	}
+
+	private void logWarn(HttpServletRequest request, Exception exception) {
+		StringWriter stringWriter = new StringWriter();
+		PrintWriter printWriter = new PrintWriter(stringWriter);
+
+		exception.printStackTrace(printWriter);
+		String stackTrace = stringWriter.toString();
+
+		log.warn("{} {} (traceId: {})\n{}", request.getMethod(), request.getRequestURI(), getTraceId(), stackTrace);
+	}
+
+	private String getTraceId() {
+		return MDC.get("traceId");
 	}
 }
