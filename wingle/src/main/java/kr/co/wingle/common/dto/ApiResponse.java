ef62d8a793a3ef1373273b7d505d58be0c90c548
silver <silver36c@gmail.com>
2023-01-30 15:22:10 +0900

    [style] import 위치 변경 #11

--- wingle/src/main/java/kr/co/wingle/common/dto/ApiResponse.java
+++ wingle/src/main/java/kr/co/wingle/common/dto/ApiResponse.java
@@ -1,37 +1,38 @@
 package kr.co.wingle.common.dto;
 
+import org.springframework.http.HttpStatus;
+
 import kr.co.wingle.common.constants.ErrorCode;
 import kr.co.wingle.common.constants.SuccessCode;
 import lombok.Getter;
-import org.springframework.http.HttpStatus;
 
 @Getter
 public class ApiResponse<T> {
-    private int status;
-    private String message;
-    private T data;
+	private int status;
+	private String message;
+	private T data;
 
-    private ApiResponse(int status, String message, T data) {
-        this.status = status;
-        this.message = message;
-        this.data = data;
-    }
+	private ApiResponse(int status, String message, T data) {
+		this.status = status;
+		this.message = message;
+		this.data = data;
+	}
 
-    private ApiResponse(int status, String message) {
-        this.status = status;
-        this.message = message;
-    }
+	private ApiResponse(int status, String message) {
+		this.status = status;
+		this.message = message;
+	}
 
-    public static <T> ApiResponse<T> success(SuccessCode code, T data) {
-        return new ApiResponse<>(code.getStatus().value(), code.getMessage(), data);
-    }
+	public static <T> ApiResponse<T> success(SuccessCode code, T data) {
+		return new ApiResponse<>(code.getStatus().value(), code.getMessage(), data);
+	}
 
-    public static <T> ApiResponse<T> error(ErrorCode code) {
-        return new ApiResponse<>(code.getStatus().value(), code.getMessage());
-    }
+	public static <T> ApiResponse<T> error(ErrorCode code) {
+		return new ApiResponse<>(code.getStatus().value(), code.getMessage());
+	}
 
-    // valid 에러
-    public static <T> ApiResponse<T> error(HttpStatus status, String message) {
-        return new ApiResponse<>(status.value(), message);
-    }
+	// valid 에러
+	public static <T> ApiResponse<T> error(HttpStatus status, String message) {
+		return new ApiResponse<>(status.value(), message);
+	}
 }
