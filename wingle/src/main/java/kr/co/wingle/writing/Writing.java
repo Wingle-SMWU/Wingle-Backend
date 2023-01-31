8bbfe22047ef0447b997739c0bcc6dfc599cb020
silver <silver36c@gmail.com>
2023-02-01 02:19:51 +0900

    [feat] 글쓰기 객체 생성 #16

---
+++ wingle/src/main/java/kr/co/wingle/writing/Writing.java
@@ -0,0 +1,33 @@
+package kr.co.wingle.writing;
+
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.ManyToOne;
+
+import kr.co.wingle.common.entity.BaseEntity;
+import kr.co.wingle.member.Member;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+@DiscriminatorColumn // 하위 테이블의 구분 컬럼 생성(default = DTYPE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Writing extends BaseEntity {
+	@ManyToOne(fetch = FetchType.LAZY)
+	@Getter
+	private Member member;
+
+	@Column(nullable = false)
+	@Getter
+	private String content;
+
+	public static Writing createWriting(Member member, String content) {
+		return new Writing(member, content);
+	}
+}
