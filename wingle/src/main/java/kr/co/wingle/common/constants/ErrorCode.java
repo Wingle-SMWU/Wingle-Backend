91166c8f2bd76e32b7637ed7afcb073c38427ccf
yujin113 <claire9585@sookmyung.ac.kr>
2023-02-03 12:04:46 +0900

    [feat] S3 관련 에러코드 추가 #24

--- wingle/src/main/java/kr/co/wingle/common/constants/ErrorCode.java
+++ wingle/src/main/java/kr/co/wingle/common/constants/ErrorCode.java
@@ -12,7 +12,10 @@ import lombok.Getter;
 public enum ErrorCode {
 	EXAMPLE_ERROR(BAD_REQUEST, "예시 실패"),
 	NO_ID(BAD_REQUEST, "존재하지 않는 id 입니다"),
-	ALREADY_DELETED(BAD_REQUEST, "이미 삭제된 값입니다");
+	ALREADY_DELETED(BAD_REQUEST, "이미 삭제된 값입니다"),
+	BAD_FILE_NAME(BAD_REQUEST, "파일 이름이 올바르지 않습니다."),
+	BAD_FILE_EXTENSION(BAD_REQUEST, "지원하지 않는 파일 형식입니다."),
+	FILE_UPLOAD_FAIL(INTERNAL_SERVER_ERROR, "파일 업로드에 실패했습니다.");
 
 	private final HttpStatus status;
 	private final String message;
