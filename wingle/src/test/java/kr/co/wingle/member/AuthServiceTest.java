ced11951c43339f315d186369b68b14e597ab2bd
yujin113 <claire9585@sookmyung.ac.kr>
2023-02-03 23:22:40 +0900

    [test] 회원가입 테스트코드 작성 #15
    - controller test
    - service test
    - 유저템플릿 생성
    - 테스트이미지 추가

---
+++ wingle/src/test/java/kr/co/wingle/member/AuthServiceTest.java
@@ -0,0 +1,54 @@
+package kr.co.wingle.member;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import kr.co.wingle.common.constants.ErrorCode;
+import kr.co.wingle.common.exception.CustomException;
+import kr.co.wingle.member.dto.SignupRequestDto;
+import kr.co.wingle.member.dto.SignupResponseDto;
+
+@SpringBootTest
+class AuthServiceTest {
+	@Autowired
+	private AuthService authService;
+	@Autowired
+	private MemberRepository memberRepository;
+
+	@AfterEach
+	void deleteMember() {
+		memberRepository.deleteAll();
+	}
+
+	@Test
+	void 회원가입() throws Exception {
+		//given
+		SignupRequestDto requestDto = MemberTemplate.makeTestSignUpRequestDto();
+
+		//when
+		SignupResponseDto response = authService.signup(requestDto);
+
+		//then
+		Member member = memberRepository.findById(response.getId())
+			.orElseThrow(() -> new IllegalArgumentException(ErrorCode.NO_ID.getMessage()));
+
+		assertThat(member.getName()).isEqualTo(response.getName());
+	}
+
+	@Test
+	void 회원가입_이메일_중복() throws Exception {
+		//given
+		Member member = MemberTemplate.makeTestMember();
+		SignupRequestDto requestDto = MemberTemplate.makeTestSignUpRequestDto();
+
+		//when
+		memberRepository.save(member);
+
+		//then
+		assertThatThrownBy(() -> authService.signup(requestDto)).isInstanceOf(CustomException.class);
+	}
+}
