370ca701ef03551b77051db23e24f2f3f1c3e9ef
yujin113 <claire9585@sookmyung.ac.kr>
2023-02-04 15:33:13 +0900

    [feat] 인증 에러코드 추가 #15

--- wingle/src/main/java/kr/co/wingle/common/constants/ErrorCode.java
+++ wingle/src/main/java/kr/co/wingle/common/constants/ErrorCode.java
@@ -17,6 +17,19 @@ public enum ErrorCode {
 	BAD_FILE_EXTENSION(BAD_REQUEST, "지원하지 않는 파일 형식입니다."),
 	EMAIL_BAD_REQUEST(BAD_REQUEST, "이메일 형식이 유효하지 않습니다."),
 	DUPLICATE_EMAIL(BAD_REQUEST, "이미 가입된 유저입니다."),
+
+	/* 401 UNAUTHORIZED: 인증 실패 */
+	UNAUTHORIZED_USER(UNAUTHORIZED, "인증된 사용자가 아닙니다."),
+	WRONG_TYPE_TOKEN(UNAUTHORIZED, "잘못된 JWT 서명을 가진 토큰입니다."),
+	EXPIRED_TOKEN(UNAUTHORIZED, "만료된 JWT 토큰입니다."),
+	UNSUPPORTED_TOKEN(UNAUTHORIZED, "지원하지 않는 JWT 토큰입니다."),
+	WRONG_TOKEN(UNAUTHORIZED, "잘못된 JWT 토큰입니다."),
+	UNKNOWN_ERROR(UNAUTHORIZED, "알 수 없는 요청 인증 에러입니다. 헤더에 토큰을 넣어 보냈는지 확인해주세요."),
+	ACCESS_DENIED(UNAUTHORIZED, "접근이 거절되었습니다."),
+
+	/* 404 NOT_FOUND: 리소스를 찾을 수 없음 */
+	USER_NOT_FOUND(NOT_FOUND, "유저 정보가 없습니다."),
+
 	FILE_UPLOAD_FAIL(INTERNAL_SERVER_ERROR, "파일 업로드에 실패했습니다.");
 
 	private final HttpStatus status;
