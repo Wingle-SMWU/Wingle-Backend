9c5bcc04016286f17678c6b269142490b059c8a2
yujin113 <claire9585@sookmyung.ac.kr>
2023-02-04 15:43:48 +0900

    [feat] 회원가입 로직 구현 #15

--- wingle/src/main/java/kr/co/wingle/member/MemberRepository.java
+++ wingle/src/main/java/kr/co/wingle/member/MemberRepository.java
@@ -1,6 +1,11 @@
 package kr.co.wingle.member;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+	boolean existsByEmail(String email);
+
+	Optional<Member> findByEmail(String email);
 }
