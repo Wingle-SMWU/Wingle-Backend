b82079cb33388fee3d77c1a7593cb87e79384e4f
KimRiun <kimriun27@gmail.com>
2023-02-04 16:49:16 +0900

    [test] 이미 등록된 이메일 중복 검사 로직 구현 # 27

--- wingle/src/test/java/kr/co/wingle/MemberServiceTest.java
+++ wingle/src/test/java/kr/co/wingle/MemberServiceTest.java
@@ -1,8 +1,12 @@
 package kr.co.wingle;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import kr.co.wingle.member.Authority;
+import kr.co.wingle.member.Member;
 import kr.co.wingle.member.MemberRepository;
 import kr.co.wingle.member.MemberService;
 
@@ -14,4 +18,25 @@ public class MemberServiceTest {
 	@Autowired
 	MemberRepository memberRepository;
 
+	@Test
+	void 이미_등록된_이메일_중복_검사() {
+		// given
+		final String email = "wingle@gmail.com";
+		Member inputMember = null;
+		boolean isMemberExisted = false;
+
+		// when
+		isMemberExisted = memberRepository.existsByEmail("wingle@gmail.com");
+		if (!isMemberExisted) {
+			inputMember = Member.createMember("", "", email, "", Authority.ROLE_USER);
+			memberRepository.save(inputMember);
+		}
+
+		// then
+		Assertions.assertEquals(true, memberService.isDuplicated(email));
+
+		// teardown
+		if (!isMemberExisted)
+			memberRepository.deleteById(inputMember.getId());
+	}
 }
