b42dc4d0ba0fccf299de16f8971fcdf3bc448b2e
KimRiun <kimriun27@gmail.com>
2023-02-16 00:16:13 +0900

    [feat] 메일 발송에 필요한 상수 설정 #44

---
+++ wingle/src/main/java/kr/co/wingle/common/config/MailConfig.java
@@ -0,0 +1,15 @@
+package kr.co.wingle.common.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.Getter;
+
+@Getter
+@Configuration
+public class MailConfig {
+	@Value("${spring.mail.username}")
+	private String username;
+	private final String title = "윙글(Wingle) 이메일 인증코드";
+	private final String name = "wingle";
+}
