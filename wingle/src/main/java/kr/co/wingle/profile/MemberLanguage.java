355f9a9c138e3f4363a93830520cbb5dffa4a370
조용은 <gdakate1215@gmail.com>
2023-02-15 12:36:19 +0900

    [feat] profile entity들 생성#23

---
+++ wingle/src/main/java/kr/co/wingle/profile/MemberLanguage.java
@@ -0,0 +1,34 @@
+package kr.co.wingle.profile;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import kr.co.wingle.common.entity.BaseEntity;
+import kr.co.wingle.member.Member;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "member_language")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class MemberLanguage extends BaseEntity {
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name= "member_id")
+	private Member member;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "language_id")
+	private Language language;
+
+	public static MemberLanguage createMemberSns(Member member, Language language){
+		return new MemberLanguage (member,language);
+	}
+}
