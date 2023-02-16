494432888c992fbc889c360f7d2c8c8c77c50078
KimRiun <kimriun27@gmail.com>
2023-02-16 18:06:11 +0900

    [feat] 인증 정보를 확인하기 위한 Dto 생성 #47

---
+++ wingle/src/main/java/kr/co/wingle/member/dto/CertificationRequestDto.java
@@ -0,0 +1,18 @@
+package kr.co.wingle.member.dto;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CertificationRequestDto {
+	@Email
+	@NotBlank(message = "이메일이 없습니다.")
+	String certificationKey;
+	@NotBlank(message = "인증정보가 없습니다.")
+	String certificationCode;
+}
