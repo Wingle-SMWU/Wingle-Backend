592d8be5def383273b40f3b6c690f8fa17afe3fe
KimRiun <kimriun27@gmail.com>
2023-02-16 10:05:48 +0900

    [fix] 세미콜론으로 인한 버그 수정 #44

--- wingle/src/main/java/kr/co/wingle/common/constants/SuccessCode.java
+++ wingle/src/main/java/kr/co/wingle/common/constants/SuccessCode.java
@@ -1,22 +1,23 @@
 package kr.co.wingle.common.constants;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
+import static org.springframework.http.HttpStatus.*;
+
 import org.springframework.http.HttpStatus;
 
-import static org.springframework.http.HttpStatus.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public enum SuccessCode {
-    EXAMPLE_SUCCESS(OK, "예시 성공"),
-    SIGNUP_SUCCESS(OK, "회원가입 성공"),
-    LOGIN_SUCCESS(OK, "로그인 성공"),
-    LOGOUT_SUCCESS(OK, "로그아웃 성공"),
-    ACCOUNT_READ_SUCCESS(OK, "계정 조회 성공"),
-    TOKEN_REISSUE_SUCCESS(OK, "토큰 재발급 성공");
+	EXAMPLE_SUCCESS(OK, "예시 성공"),
+	SIGNUP_SUCCESS(OK, "회원가입 성공"),
+	LOGIN_SUCCESS(OK, "로그인 성공"),
+	LOGOUT_SUCCESS(OK, "로그아웃 성공"),
+	ACCOUNT_READ_SUCCESS(OK, "계정 조회 성공"),
+	TOKEN_REISSUE_SUCCESS(OK, "토큰 재발급 성공"),
 	EMAIL_SEND_SUCCESS(OK, "이메일 인증코드 전송 성공");
 
-    private final HttpStatus status;
-    private final String message;
+	private final HttpStatus status;
+	private final String message;
 }
