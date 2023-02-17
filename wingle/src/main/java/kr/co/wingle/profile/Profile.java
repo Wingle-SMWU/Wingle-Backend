75fdbad33fc8269c6155d53b53a004a1202fa861
yujin113 <claire9585@sookmyung.ac.kr>
2023-02-18 01:40:29 +0900

    [fix] Profile 엔티티 수정 #53

--- wingle/src/main/java/kr/co/wingle/profile/Profile.java
+++ wingle/src/main/java/kr/co/wingle/profile/Profile.java
@@ -2,11 +2,15 @@ package kr.co.wingle.profile;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.ColumnDefault;
 
 import kr.co.wingle.common.entity.BaseEntity;
+import kr.co.wingle.member.entity.Member;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,6 +21,10 @@ import lombok.Setter;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Profile extends BaseEntity {
+
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id")
+	private Member member;
 
 	@Column(nullable = false)
 	private String nickname;
@@ -32,13 +40,18 @@ public class Profile extends BaseEntity {
 	@Setter
 	private boolean registration;
 
-	public static Profile createProfile(String nickname, String introduction, boolean gender, boolean registration) {
+	@Column(nullable = false)
+	private String nation;
+
+	public static Profile createProfile(Member member, String nickname, String introduction, boolean gender,
+		boolean registration, String nation) {
 		Profile profile = new Profile();
+		profile.member = member;
 		profile.nickname = nickname;
 		profile.introduction = introduction;
 		profile.gender = gender;
 		profile.registration = registration;
-
+		profile.nation = nation;
 		return profile;
 	}
 
