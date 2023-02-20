708bcdc25351501450b3006258da6d30fe13ad94
KimRiun <kimriun27@gmail.com>
2023-02-20 21:56:56 +0900

    [feat] 쪽지 서비스 생성 #22

---
+++ wingle/src/main/java/kr/co/wingle/message/MessageService.java
@@ -0,0 +1,10 @@
+package kr.co.wingle.message;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class MessageService {
+}
