16e094ac720cdfbcacd3aca219896473d2c8138a
silver <silver36c@gmail.com>
2023-03-01 07:14:37 +0900

    [feat] 댓글 삭제 #21

--- wingle/src/main/java/kr/co/wingle/community/comment/CommentController.java
+++ wingle/src/main/java/kr/co/wingle/community/comment/CommentController.java
@@ -2,13 +2,16 @@ package kr.co.wingle.community.comment;
 
 import javax.validation.Valid;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import kr.co.wingle.common.constants.SuccessCode;
 import kr.co.wingle.common.dto.ApiResponse;
+import kr.co.wingle.common.util.StringUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -23,5 +26,14 @@ public class CommentController {
 	public ApiResponse<CommentResponseDto> create(@ModelAttribute @Valid CommentRequestDto request) {
 		CommentResponseDto response = commentService.create(request);
 		return ApiResponse.success(SuccessCode.COMMENT_CREATE_SUCCESS, response);
+	}
+
+	@DeleteMapping("/{forumId}/articles/{articleId}/comments/{commentId}")
+	public ApiResponse<Long> delete(@PathVariable String forumId,
+		@PathVariable String articleId, @PathVariable String commentId) {
+
+		return ApiResponse.success(SuccessCode.COMMENT_DELETE_SUCCESS,
+			commentService.delete(StringUtil.StringToLong(forumId), StringUtil.StringToLong(articleId),
+				StringUtil.StringToLong(commentId)));
 	}
 }
--- wingle/src/main/java/kr/co/wingle/community/comment/CommentService.java
+++ wingle/src/main/java/kr/co/wingle/community/comment/CommentService.java
@@ -3,15 +3,18 @@ package kr.co.wingle.community.comment;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import kr.co.wingle.common.constants.ErrorCode;
+import kr.co.wingle.common.exception.NotFoundException;
 import kr.co.wingle.community.article.Article;
 import kr.co.wingle.community.article.ArticleService;
+import kr.co.wingle.community.writing.WritingService;
 import kr.co.wingle.member.entity.Member;
 import kr.co.wingle.member.service.AuthService;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Service
-public class CommentService {
+public class CommentService extends WritingService {
 	private final ArticleService articleService;
 	private final CommentMapper commentMapper;
 	private final AuthService authService;
@@ -27,5 +30,35 @@ public class CommentService {
 		commentRepository.save(comment);
 		return commentMapper.toResponseDto(comment);
 
+	}
+
+	@Transactional
+	public Long delete(Long forumId, Long articleId, Long commentId) {
+		Member member = authService.findMember();
+		Comment comment = getCommentById(commentId);
+
+		if (isValidMember(comment, member) && isExist(comment) && articleService.isValidForum(comment.getArticle(),
+			forumId) && isValidArticle(comment, articleId)) {
+			comment.softDelete();
+		}
+		return comment.getId();
+	}
+
+	@Transactional(readOnly = true)
+	public Comment getCommentById(Long commentId) {
+		Comment comment = commentRepository.findById(commentId)
+			.orElseThrow(() -> new NotFoundException(
+				ErrorCode.NO_ARTICLE_ID));
+		isExist(comment);
+		return comment;
+	}
+
+	@Transactional(readOnly = true)
+	public boolean isValidArticle(Comment comment, Long articleId) {
+		// 게시글 안 맞으면 에러
+		if (comment.getArticle().getId() != articleId) {
+			throw new NotFoundException(ErrorCode.BAD_PARAMETER);
+		}
+		return true;
 	}
 }
