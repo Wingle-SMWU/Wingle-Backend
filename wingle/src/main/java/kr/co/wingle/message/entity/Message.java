39a04b977839517c4bf05e15c9de2ea7e373c973
silver <silver36c@gmail.com>
2023-03-03 11:57:14 +0900

    [feat] Message 엔티티 Writing 상속받게 수정 #22

--- wingle/src/main/java/kr/co/wingle/message/entity/Message.java
+++ wingle/src/main/java/kr/co/wingle/message/entity/Message.java
@@ -1,14 +1,12 @@
 package kr.co.wingle.message.entity;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
-import kr.co.wingle.common.entity.BaseEntity;
-import kr.co.wingle.member.entity.Member;
+import kr.co.wingle.writing.Writing;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,15 +15,8 @@ import lombok.NoArgsConstructor;
 @Table(name = "message")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Message extends BaseEntity {
+public class Message extends Writing {
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "room_id", nullable = false)
 	private Room room;
-
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "member_id", nullable = false)
-	private Member member;
-
-	@Column(nullable = false, length = 1000)
-	private String content;
 }
