8ec2348036f02f1dc178ab2711edff7a56aaafb7
silver <silver36c@gmail.com>
2023-03-03 15:45:54 +0900

    [fix] 오타 수정 #22

--- wingle/src/main/java/kr/co/wingle/message/repository/RoomMemberRepository.java
+++ wingle/src/main/java/kr/co/wingle/message/repository/RoomMemberRepository.java
@@ -2,5 +2,7 @@ package kr.co.wingle.message.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RoomMemberRepository extends JpaRepository<RoomMemberRepository, Long> {
+import kr.co.wingle.message.entity.RoomMember;
+
+public interface RoomMemberRepository extends JpaRepository<RoomMember, Long> {
 }
