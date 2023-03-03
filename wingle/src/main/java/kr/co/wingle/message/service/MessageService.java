63ef9b5d049df4159bcbd8e72c068a717c8353a8
silver <silver36c@gmail.com>
2023-03-04 02:36:15 +0900

    [feat] 자기자신과 쪽지 방지 #22

--- wingle/src/main/java/kr/co/wingle/message/service/MessageService.java
+++ wingle/src/main/java/kr/co/wingle/message/service/MessageService.java
@@ -3,6 +3,8 @@ package kr.co.wingle.message.service;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import kr.co.wingle.common.constants.ErrorCode;
+import kr.co.wingle.common.exception.NotFoundException;
 import kr.co.wingle.member.entity.Member;
 import kr.co.wingle.member.service.AuthService;
 import kr.co.wingle.message.dto.MessageRequestDto;
@@ -11,6 +13,7 @@ import kr.co.wingle.message.entity.Message;
 import kr.co.wingle.message.entity.Room;
 import kr.co.wingle.message.mapper.MessageMapper;
 import kr.co.wingle.message.repository.MessageRepository;
+import kr.co.wingle.message.repository.RoomMemberRepository;
 import kr.co.wingle.writing.WritingService;
 import lombok.RequiredArgsConstructor;
 
@@ -18,6 +21,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class MessageService extends WritingService {
 	private final MessageRepository messageRepository;
+	private final RoomMemberRepository roomMemberRepository;
 	private final MessageMapper messageMapper;
 	private final AuthService authService;
 	private final RoomService roomService;
@@ -26,6 +30,12 @@ public class MessageService extends WritingService {
 	public MessageResponseDto send(MessageRequestDto messageRequestDto) {
 		Member member = authService.findMember();
 		Room room = roomService.getRoomById(messageRequestDto.getRoomId());
+		// 해당 쪽지방에 있는지 검사
+		roomMemberRepository.findAllByRoomIdAndMemberIdAndIsDeleted(room.getId(), member.getId(),
+				false)
+			.orElseThrow(() -> new NotFoundException(
+				ErrorCode.FORBIDDEN_USER));
+
 		Message message = Message.of(member, messageRequestDto.getContent(), room);
 		messageRepository.save(message);
 		return messageMapper.toResponseDto(message);
