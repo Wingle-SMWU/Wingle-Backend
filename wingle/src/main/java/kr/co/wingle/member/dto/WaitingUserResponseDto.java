37df9d8bcdd7610c3e6455468f95885f72b09219
Jisu Lee <suen0904@gmail.com>
2023-06-01 00:02:44 +0900

    [refactor] admin userId 암호화

--- wingle/src/main/java/kr/co/wingle/member/dto/WaitingUserResponseDto.java
+++ wingle/src/main/java/kr/co/wingle/member/dto/WaitingUserResponseDto.java
@@ -2,6 +2,7 @@ package kr.co.wingle.member.dto;
 
 import java.time.LocalDateTime;
 
+import kr.co.wingle.common.util.AES256Util;
 import kr.co.wingle.member.entity.Member;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -10,7 +11,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class WaitingUserResponseDto {
-	private Long userId;
+	private String userId;
 	private String name;
 	private LocalDateTime createdTime;
 	private String idCardImage;
@@ -20,7 +21,7 @@ public class WaitingUserResponseDto {
 
 	public static WaitingUserResponseDto from(Member member, String nation) {
 		WaitingUserResponseDto waitingUserResponseDto = new WaitingUserResponseDto();
-		waitingUserResponseDto.userId = member.getId();
+		waitingUserResponseDto.userId = AES256Util.encrypt(member.getId().toString());
 		waitingUserResponseDto.name = member.getName();
 		waitingUserResponseDto.createdTime = member.getCreatedTime();
 		waitingUserResponseDto.idCardImage = member.getIdCardImageUrl();
