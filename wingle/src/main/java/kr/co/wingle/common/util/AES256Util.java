7eae504f469fa9d71af7c6dd33a586585807800f
Jisu Lee <suen0904@gmail.com>
2023-06-01 00:02:22 +0900

    [refactor] base64 -> hex로 변경, static 추가

--- wingle/src/main/java/kr/co/wingle/common/util/AES256Util.java
+++ wingle/src/main/java/kr/co/wingle/common/util/AES256Util.java
@@ -8,7 +8,6 @@ import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
-import org.apache.commons.codec.binary.Base64;
 import org.springframework.stereotype.Component;
 
 import kr.co.wingle.common.constants.ErrorCode;
@@ -17,11 +16,11 @@ import kr.co.wingle.common.exception.CustomException;
 @Component
 public class AES256Util {
 	//initial vector 설정
-	private String iv = "0000000000000001";
-	private Key keySpec;
+	private static String iv = "0000000000000001";
+	private static Key keySpec;
 
 	public AES256Util() throws UnsupportedEncodingException {
-		this.iv = iv.substring(0, 16);
+		iv = iv.substring(0, 16);
 		byte[] keyBytes = new byte[16];
 		byte[] b = iv.getBytes("UTF-8");
 		int len = b.length;
@@ -29,29 +28,30 @@ public class AES256Util {
 			len = keyBytes.length;
 		}
 		System.arraycopy(b, 0, keyBytes, 0, len);
-		SecretKeySpec keySpec = new SecretKeySpec(keyBytes, "AES");
-		this.keySpec = keySpec;
+		SecretKeySpec _keySpec = new SecretKeySpec(keyBytes, "AES");
+		keySpec = _keySpec;
 	}
 
 	//암호화
-	public String encrypt(String str) {
+	public static String encrypt(String str) {
 		try {
 			Cipher c = Cipher.getInstance("AES/CBC/PKCS5Padding");
 			c.init(Cipher.ENCRYPT_MODE, keySpec, new IvParameterSpec((iv.getBytes())));
 			byte[] encrypted = c.doFinal(str.getBytes("UTF-8"));
-			String enStr = new String(Base64.encodeBase64(encrypted));
-			return enStr;
+			// String enStr = new String(Base64.encodeBase64(encrypted));
+			return new java.math.BigInteger(encrypted).toString(16);
 		} catch (GeneralSecurityException | UnsupportedEncodingException e) {
 			throw new CustomException(ErrorCode.ENCRYPT_FAIL);
 		}
 	}
 
 	//복호화
-	public String decrypt(String str) {
+	public static String decrypt(String str) {
 		try {
 			Cipher c = Cipher.getInstance("AES/CBC/PKCS5Padding");
 			c.init(Cipher.DECRYPT_MODE, keySpec, new IvParameterSpec(iv.getBytes()));
-			byte[] byteStr = Base64.decodeBase64(str.getBytes());
+			// byte[] byteStr = Base64.decodeBase64(str.getBytes());
+			byte[] byteStr = new java.math.BigInteger(str, 16).toByteArray();
 			return new String(c.doFinal(byteStr), "UTF-8");
 		} catch (GeneralSecurityException | UnsupportedEncodingException e) {
 			throw new CustomException(ErrorCode.DECRYPT_FAIL);
