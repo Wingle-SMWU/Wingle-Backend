56c126d0215cd3d1fc3f2f28ac39b85d66926a5c
minjikim <inkmjj@sookmyung.ac.kr>
2023-07-02 18:43:07 +0900

    [fix] validator 수정 및 게시글 최대 3000자로 변경

--- wingle/src/main/java/kr/co/wingle/community/article/ArticleRequestDto.java
+++ wingle/src/main/java/kr/co/wingle/community/article/ArticleRequestDto.java
@@ -18,7 +18,7 @@ public class ArticleRequestDto {
 	@NotNull(message = "게시판 id가 없습니다.")
 	private Long forumId;
 
-	@LengthWithLiteralEscape(min = 1, max = 1000, message = "내용은 1자 이상 3000자 이하만 가능합니다.")
+	@LengthWithLiteralEscape(min = 1, max = 3000, message = "내용은 1자 이상 3000자 이하만 가능합니다.")
 	private String content;
 
 	private List<MultipartFile> images;
