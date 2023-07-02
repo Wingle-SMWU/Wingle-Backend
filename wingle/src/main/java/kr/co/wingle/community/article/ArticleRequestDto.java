0b781f71349e8154e06030b82ee4bbd163727768
minjikim <inkmjj@sookmyung.ac.kr>
2023-07-02 18:11:03 +0900

    [fix] 게시글, 댓글 최대글자수 변경 및 글자수 validator 수정

--- wingle/src/main/java/kr/co/wingle/community/article/ArticleRequestDto.java
+++ wingle/src/main/java/kr/co/wingle/community/article/ArticleRequestDto.java
@@ -3,10 +3,10 @@ package kr.co.wingle.community.article;
 import java.util.List;
 
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import kr.co.wingle.common.validator.LengthWithLiteralEscape;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -18,8 +18,7 @@ public class ArticleRequestDto {
 	@NotNull(message = "게시판 id가 없습니다.")
 	private Long forumId;
 
-	@NotNull(message = "내용이 없습니다.")
-	@Size(min = 1, max = 1000, message = "내용은 1자 이상 1000자 이하만 가능합니다.")
+	@LengthWithLiteralEscape(min = 1, max = 1000, message = "내용은 1자 이상 3000자 이하만 가능합니다.")
 	private String content;
 
 	private List<MultipartFile> images;
