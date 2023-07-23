0e7bac78187d8c61e94c012570dcbfd7d04f307c
silver <silver36c@gmail.com>
2023-07-23 21:00:52 +0900

    [fix] 회원가입 결과 user id 암호화 #237

--- wingle/src/main/java/kr/co/wingle/member/dto/SignupResponseDto.java
+++ wingle/src/main/java/kr/co/wingle/member/dto/SignupResponseDto.java
@@ -1,5 +1,6 @@
 package kr.co.wingle.member.dto;
 
+import kr.co.wingle.common.util.AES256Util;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -7,13 +8,13 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class SignupResponseDto {
-	private Long id;
+	private String id;
 	private String name;
 	private String nickname;
 
 	public static SignupResponseDto of(Long id, String name, String nickname) {
 		SignupResponseDto responseDto = new SignupResponseDto();
-		responseDto.id = id;
+		responseDto.id = AES256Util.encrypt(id.toString());
 		responseDto.name = name;
 		responseDto.nickname = nickname;
 		return responseDto;
