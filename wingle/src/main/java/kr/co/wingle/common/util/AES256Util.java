a9b6224a611d4dc4df552b74a785dd9eb1de3ff7
minjikim <inkmjj@sookmyung.ac.kr>
2023-08-22 10:59:42 +0900

    [fix] 암호화 및 복호화 시 hex 인코딩 적용

--- wingle/src/main/java/kr/co/wingle/common/util/AES256Util.java
+++ wingle/src/main/java/kr/co/wingle/common/util/AES256Util.java
@@ -8,6 +8,8 @@ import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
 import org.springframework.stereotype.Component;
 
 import kr.co.wingle.common.constants.ErrorCode;
@@ -38,8 +40,7 @@ public class AES256Util {
 			Cipher c = Cipher.getInstance("AES/CBC/PKCS5Padding");
 			c.init(Cipher.ENCRYPT_MODE, keySpec, new IvParameterSpec((iv.getBytes())));
 			byte[] encrypted = c.doFinal(str.getBytes("UTF-8"));
-			// String enStr = new String(Base64.encodeBase64(encrypted));
-			return new java.math.BigInteger(encrypted).toString(16);
+			return Hex.encodeHexString(encrypted);
 		} catch (GeneralSecurityException | UnsupportedEncodingException e) {
 			throw new InternalServerErrorException(ErrorCode.ENCRYPT_FAIL);
 		}
@@ -50,10 +51,9 @@ public class AES256Util {
 		try {
 			Cipher c = Cipher.getInstance("AES/CBC/PKCS5Padding");
 			c.init(Cipher.DECRYPT_MODE, keySpec, new IvParameterSpec(iv.getBytes()));
-			// byte[] byteStr = Base64.decodeBase64(str.getBytes());
-			byte[] byteStr = new java.math.BigInteger(str, 16).toByteArray();
+			byte[] byteStr = Hex.decodeHex(str.toCharArray());
 			return new String(c.doFinal(byteStr), "UTF-8");
-		} catch (GeneralSecurityException | UnsupportedEncodingException e) {
+		} catch (GeneralSecurityException | UnsupportedEncodingException | DecoderException e) {
 			throw new InternalServerErrorException(ErrorCode.DECRYPT_FAIL);
 		}
 	}
