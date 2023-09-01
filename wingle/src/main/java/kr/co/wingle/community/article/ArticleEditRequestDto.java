0f2dba740746c85ff8033ae135e1f834543e4df7
minjikim <inkmjj@sookmyung.ac.kr>
2023-09-01 10:59:08 +0900

    [add] 게시글 수정 req dto notnull validator 추가

--- wingle/src/main/java/kr/co/wingle/community/article/ArticleEditRequestDto.java
+++ wingle/src/main/java/kr/co/wingle/community/article/ArticleEditRequestDto.java
@@ -2,6 +2,8 @@ package kr.co.wingle.community.article;
 
 import java.util.List;
 
+import javax.validation.constraints.NotNull;
+
 import org.springframework.web.multipart.MultipartFile;
 
 import kr.co.wingle.common.validator.LengthWithoutCR;
@@ -17,7 +19,10 @@ public class ArticleEditRequestDto {
 	@LengthWithoutCR(min = 1, max = 3000, message = "내용은 1자 이상 3000자 이하만 가능합니다.")
 	private String content;
 
+	@NotNull(message = "originImages가 없습니다.")
 	private List<String> originImages;
+
+	@NotNull(message = "newImages가 없습니다.")
 	private List<MultipartFile> newImages;
 
 }
